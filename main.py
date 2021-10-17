a13c87b4e7f575b2e64c5e6aa94759c4d9cb865d
Julien <cefadrom1@gmail.com>
2021-10-17 22:50:28 +0200

    feat: add ship on a board

--- main.py
+++ main.py
@@ -20,19 +20,59 @@ def display_board(board):
     print('')
 
 
-# ---------- GAME LOOP ----------
+# ---------- SHIPS HELPERS ----------
 
 ships = [
-    ["porte-avion", 1, 5],
-    ["croiseur", 2, 4],
-    ["contre-torpilleurs", 3, 3],
-    ["sous-marin", 4, 3],
-    ["torpilleur", 5, 2],
+    ['porte-avion', 1, 5],
+    ['croiseur', 2, 4],
+    ['contre-torpilleurs', 3, 3],
+    ['sous-marin', 4, 3],
+    ['torpilleu', 5, 2],
 ]
+
+
+def get_ship_data(ship_id):
+    for ship in ships:
+        if ship[1] == ship_id:
+            return ship
+
+
+def place_ship(board, line, col, direction, ship_id):
+    ship_size = get_ship_data(ship_id)[2]
+
+    # Getting the points where the ship should be placed
+    if direction == 'v':
+        board_region = board[line][col:(col + ship_size)]
+    else:
+        board_region = [board_line[col] for board_line in board][line:(line + ship_size)]
+    
+    # If the result region is smaller than the ship size, 
+    # it means that the end of the ship is out of bounds
+    if len(board_region) != ship_size:
+        return False
+    
+    # Tests if all points are empty 
+    if not all(point == 0 for point in board_region):
+        return False
+
+    # Place the ship
+    if direction == 'v':
+        board[line][col:(col + ship_size)] = [ship_id] * ship_size
+    else:
+        for board_line in range(10):
+            if board_line > (line - 1) and board_line < line + ship_size:
+                board[board_line][col] = ship_id
+    
+    return True
+
+
+# ---------- GAME LOOP ----------
 
 board1 = generate_board()
 board2 = generate_board()
 
 
 if __name__ == '__main__':
+    display_board(board1)
+    place_ship(board1, 5, 6, 'h', 3)
     display_board(board1)
